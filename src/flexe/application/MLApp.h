4852355ba1bb704ce9af0fd23874afa1b147d2cb
Wellington Viana Lobato Junior <wellington.lobato.junior@gmail.com>
2023-07-03 18:53:44 +0200

    Update MLApp.h

--- src/flexe/application/MLApp.h
+++ src/flexe/application/MLApp.h
@@ -30,10 +30,10 @@ class FlexeClient {
     FlexeClient(std::shared_ptr<Channel> channel)
     : stub_(Flexe::NewStub(channel)) {}
 
-    int evaluate(int idVehicle, const std::string& dataset, const std::string& modelName, int batchSize){
+    double evaluate(int idVehicle, bool trainFlag, const std::string& modelName, int batchSize){
         TTRequest evaluateRequest;
         evaluateRequest.set_idvehicle(idVehicle+1);
-        evaluateRequest.set_datasetpath(dataset);
+        evaluateRequest.set_trainflag(false);
         evaluateRequest.set_modelname(modelName);
         evaluateRequest.set_batch_size(batchSize);
 
@@ -42,7 +42,7 @@ class FlexeClient {
 
         Status status = stub_->evaluate(&context, evaluateRequest, &reply);
         if(status.ok()){
-            std::cout << "Evaluation requested successfully!" << endl;
+            //std::cout << "Evaluation requested successfully!" << endl;
             return reply.loss();
         } else {
             std::cout << status.error_code() << ": " << status.error_message() << std::endl;
@@ -50,10 +50,10 @@ class FlexeClient {
         }
     }
 
-    ModelReply fit(int idVehicle, const std::string& dataset, const std::string& modelName, int epochs, int batchSize){
+    ModelReply fit(int idVehicle, bool trainFlag, const std::string& modelName, int epochs, int batchSize){
         TTRequest fitRequest;
         fitRequest.set_idvehicle(idVehicle+1);
-        fitRequest.set_datasetpath(dataset);
+        fitRequest.set_trainflag(trainFlag);
         fitRequest.set_modelname(modelName);
         fitRequest.set_epochs(epochs);
         fitRequest.set_batch_size(batchSize);
@@ -62,17 +62,17 @@ class FlexeClient {
 
         Status status = stub_->fit(&context, fitRequest, &reply);
         if(status.ok()){
-            std::cout << "Training requested successfully!" << endl;
+            //std::cout << "Training requested successfully!" << endl;
         } else {
             std::cout << status.error_code() << ": " << status.error_message() << std::endl;
         }
         return reply;
     }
 
-    int update_model(int idVehicle, const std::string& dataset, const std::string& modelName, int epochs, int batchSize){
+    int update_model(int idVehicle, bool trainFlag, const std::string& modelName, int epochs, int batchSize){
         TTRequest fitRequest;
         fitRequest.set_idvehicle(idVehicle+1);
-        fitRequest.set_datasetpath(dataset);
+        fitRequest.set_trainflag(false);
         fitRequest.set_modelname(modelName);
         fitRequest.set_epochs(epochs);
         fitRequest.set_batch_size(batchSize);
@@ -81,11 +81,11 @@ class FlexeClient {
 
         Status status = stub_->update_model(&context, fitRequest, &reply);
         if(status.ok()){
-            std::cout << "Update the model requested successfully!" << endl;
+            //std::cout << "Update the model requested successfully!" << endl;
             return 1;
         }else {
             std::cout << status.error_code() << ": " << status.error_message() << std::endl;
-            return 0;
+            return -1;
         }
     }
 
@@ -122,15 +122,21 @@ protected:
 
     //VAR
     int self;
-    std::string dataset;
-    std::string modelName;
-    std::string strWeights;
     int epochs;
     int batchSize;
-    double trainInterval;
+    int counter;
+    int modelVersion;
+
+    bool trainFlag;
+
+    double sendTrainInterval;
+    double loss;
+
+    std::string modelName;
+    std::string strWeights;
 
     ModelReply modelResponse;
-    int counter;
+
     std::vector<int> tensorSizeVector;
 
     std::string address;
